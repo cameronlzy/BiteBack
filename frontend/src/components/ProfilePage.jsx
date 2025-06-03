ea86bc0e7889e060408babc34b711e0a30378552
Cameron Loh Zi Yang <96047195+cameronlzy@users.noreply.github.com>
2025-06-03 14:17:19 +0800

    Update ProfilePage.jsx

--- frontend/src/components/ProfilePage.jsx
+++ frontend/src/components/ProfilePage.jsx
@@ -3,6 +3,13 @@ import CustomerReservations from "@/components/CustomerReservations"
 import OwnerRestaurants from "@/components/OwnerRestaurants"
 import authService from "@/services/authService"
 import { Button } from "@/components/ui/button"
+import { Settings } from "lucide-react"
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu"
 import { useLocation, useNavigate } from "react-router-dom"
 import CustomerReviews from "./CustomerReviews"
 const ProfilePage = ({ user }) => {
@@ -12,25 +19,40 @@ const ProfilePage = ({ user }) => {
     await authService.logout()
     window.location = "/"
   }
+
+  const dropdownContent = [
+    {
+      content: "Reset Password",
+      onClick: () => {
+        navigate("forgot-password", { replace: true })
+      },
+    },
+    {
+      content: "Edit Profile",
+      onClick: () =>
+        navigate("/me/edit", { state: { from: location.pathname } }),
+    },
+    { content: "Logout", onClick: handleLogout },
+  ]
   return (
     <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="relative mb-6">
-        {/* Button Group: Top-right on md+, stacked below on mobile */}
-        <div className="absolute top-0 right-0 hidden sm:flex gap-2">
-          <Button
-            onClick={() =>
-              navigate("/me/edit", { state: { from: location.pathname } })
-            }
-            variant="ghost"
-          >
-            Edit Profile
-          </Button>
-          <Button onClick={handleLogout} variant="ghost">
-            Logout
-          </Button>
+        <div className="absolute top-0 right-0 hidden sm:flex">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon">
+                <Settings className="w-5 h-5" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              {dropdownContent.map(({ onClick, content }) => (
+                <DropdownMenuItem key={content} onClick={onClick}>
+                  {content}
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
-
-        {/* Mobile Button Group (shown only on small screens) */}
         <div className="flex sm:hidden justify-center gap-2 mt-2">
           <Button
             onClick={() =>
@@ -44,8 +66,6 @@ const ProfilePage = ({ user }) => {
             Logout
           </Button>
         </div>
-
-        {/* Centered Profile Info */}
         <div className="text-center mt-2">
           <h1 className="text-3xl font-bold">
             {user.role === "customer"
