477a1f99a8dda834a70514fca7b60ca993e88415
benlua <benlua73@gmail.com>
2025-06-24 19:16:39 +0800

    Fixed deployment crash

--- backend/middleware/isCustomer.js
+++ backend/middleware/isCustomer.js
@@ -1,4 +1,4 @@
-import { wrapError } from '../helpers/response';
+import { wrapError } from '../helpers/response.js';
 
 export default function (req, res, next) { 
   if (req.user.role != "customer") return res.status(403).json(wrapError('Access denied: Only customers allowed'));
